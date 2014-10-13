7214764dd52f2c0ddcba76babc0c45fab3c62156
Andreas Hallberg <anhallbe@gmail.com>
2014-10-13 18:38:16 +0200

    shortest improved

--- src/walker_improved.pl
+++ src/walker_improved.pl
@@ -2,20 +2,28 @@
 
 %Find a path from Start to End through a NxN world.
 find(Start, End, N, Path, Steps) :-
-        walk(Start, End, N, [], Path),
+        findall(X, walk(Start, End, N, [], X), All),
+        shortest_path(All, Shortest),
+        reverse(Shortest, Path),
         length(Path, Steps).
+        %walk(Start, End, N, [], Path),
+        %length(Path, Steps).
 
+%Base: Found the goal, no need to walk further.
 walk((Xs, Ys), (Xe, Ye), _, Walked, Path) :-
         Xs =:= Xe,
         Ys =:= Ye,
-        reverse(Walked, Path).
+        Path = Walked.
+        %reverse(Walked, Path).
 
+%True if I can move in any direction
 walk(Current, Goal, N, Walked, Path) :-
         walk_right(Current, Goal, N, Walked, Path);
         walk_down(Current, Goal, N, Walked, Path);
         walk_left(Current, Goal, N, Walked, Path);
         walk_up(Current, Goal, N, Walked, Path).
 
+%True if I can move to the right
 walk_right((Xc, Yc), Goal, N, Walked, Path) :-
         Xn is Xc+1,
         Yn is Yc,
@@ -23,6 +31,7 @@ walk_right((Xc, Yc), Goal, N, Walked, Path) :-
         \+memberchk((Xn, Yn), Walked),
         walk((Xn,Yn), Goal, N, [(Xn,Yn)|Walked], Path).
 
+%True if I can move down
 walk_down((Xc, Yc), Goal, N, Walked, Path) :-
         Xn is Xc,
         Yn is Yc+1,
@@ -30,6 +39,7 @@ walk_down((Xc, Yc), Goal, N, Walked, Path) :-
         \+memberchk((Xn, Yn), Walked),
         walk((Xn,Yn), Goal, N, [(Xn,Yn)|Walked], Path).
 
+%True if I can move to the left
 walk_left((Xc, Yc), Goal, N, Walked, Path) :-
         Xn is Xc-1,
         Yn is Yc,
@@ -37,9 +47,26 @@ walk_left((Xc, Yc), Goal, N, Walked, Path) :-
         \+memberchk((Xn, Yn), Walked),
         walk((Xn,Yn), Goal, N, [(Xn,Yn)|Walked], Path).
 
+%True if I can move up
 walk_up((Xc, Yc), Goal, N, Walked, Path) :-
         Xn is Xc,
         Yn is Yc-1,
         Yn > 0,
         \+memberchk((Xn, Yn), Walked),
         walk((Xn,Yn), Goal, N, [(Xn,Yn)|Walked], Path).
+
+
+%Find the Shortest path (list)
+shortest_path([P|T], Shortest) :-
+        shortest_path(T, P, Shortest).
+
+%Helper predicates
+shortest_path([], Shorter, Res) :-
+        Res = Shorter.
+
+shortest_path([Shorter|Rest], Longer, R) :-
+        shorter_list(Shorter, Longer),
+        shortest_path(Rest, Shorter, R).
+
+shortest_path([_|Rest], Shorter, R) :-
+        shortest_path(Rest, Shorter, R).
