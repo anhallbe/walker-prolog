248c225ac38fe032b790322ff787065443f71ea2
Andreas Hallberg <anhallbe@gmail.com>
2014-10-13 19:10:47 +0200

    added obstacles

--- src/walker_improved.pl
+++ src/walker_improved.pl
@@ -1,9 +1,9 @@
 :- use_module(library(lists)).
 
 %Find a path from Start to End through a NxN world.
-find(Start, End, N, Path, Steps) :-
+find(Start, End, Obstacles, N, Path, Steps) :-
         print('Looking for paths...\n'),
-        findall(X, walk(Start, End, N, [], X), All),
+        findall(X, walk(Start, End, Obstacles, N, [], X), All),
         
         print('Found '),
         length(All, Total),
@@ -23,50 +23,54 @@ find(Start, End, N, Path, Steps) :-
         %length(Path, Steps).
 
 %Base: Found the goal, no need to walk further.
-walk((Xs, Ys), (Xe, Ye), _, Walked, Path) :-
+walk((Xs, Ys), (Xe, Ye), _, _, Walked, Path) :-
         Xs =:= Xe,
         Ys =:= Ye,
         Path = Walked.
         %reverse(Walked, Path).
 
 %True if I can move in any direction
-walk(Current, Goal, N, Walked, Path) :-
-        walk_right(Current, Goal, N, Walked, Path);
-        walk_down(Current, Goal, N, Walked, Path);
-        walk_left(Current, Goal, N, Walked, Path);
-        walk_up(Current, Goal, N, Walked, Path).
+walk(Current, Goal, Obstacles, N, Walked, Path) :-
+        walk_right(Current, Goal, Obstacles, N, Walked, Path);
+        walk_down(Current, Goal, Obstacles, N, Walked, Path);
+        walk_left(Current, Goal, Obstacles, N, Walked, Path);
+        walk_up(Current, Goal, Obstacles, N, Walked, Path).
 
 %True if I can move to the right
-walk_right((Xc, Yc), Goal, N, Walked, Path) :-
+walk_right((Xc, Yc), Goal, Obstacles, N, Walked, Path) :-
         Xn is Xc+1,
         Yn is Yc,
         Xn < N,
         \+memberchk((Xn, Yn), Walked),
-        walk((Xn,Yn), Goal, N, [(Xn,Yn)|Walked], Path).
+        \+memberchk((Xn, Yn), Obstacles),
+        walk((Xn,Yn), Goal, Obstacles, N, [(Xn,Yn)|Walked], Path).
 
 %True if I can move down
-walk_down((Xc, Yc), Goal, N, Walked, Path) :-
+walk_down((Xc, Yc), Goal, Obstacles, N, Walked, Path) :-
         Xn is Xc,
         Yn is Yc+1,
         Yn < N,
         \+memberchk((Xn, Yn), Walked),
-        walk((Xn,Yn), Goal, N, [(Xn,Yn)|Walked], Path).
+        \+memberchk((Xn, Yn), Obstacles),
+        walk((Xn,Yn), Goal, Obstacles, N, [(Xn,Yn)|Walked], Path).
 
 %True if I can move to the left
-walk_left((Xc, Yc), Goal, N, Walked, Path) :-
+walk_left((Xc, Yc), Goal, Obstacles, N, Walked, Path) :-
         Xn is Xc-1,
         Yn is Yc,
         Xn > 0,
         \+memberchk((Xn, Yn), Walked),
-        walk((Xn,Yn), Goal, N, [(Xn,Yn)|Walked], Path).
+        \+memberchk((Xn, Yn), Obstacles),
+        walk((Xn,Yn), Goal, Obstacles, N, [(Xn,Yn)|Walked], Path).
 
 %True if I can move up
-walk_up((Xc, Yc), Goal, N, Walked, Path) :-
+walk_up((Xc, Yc), Goal, Obstacles, N, Walked, Path) :-
         Xn is Xc,
         Yn is Yc-1,
         Yn > 0,
         \+memberchk((Xn, Yn), Walked),
-        walk((Xn,Yn), Goal, N, [(Xn,Yn)|Walked], Path).
+        \+memberchk((Xn, Yn), Obstacles),
+        walk((Xn,Yn), Goal, Obstacles, N, [(Xn,Yn)|Walked], Path).
 
 
 %Find the Shortest path (list)
